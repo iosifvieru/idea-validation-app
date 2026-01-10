db43d24a03957de79e9d0f4bb108c6fbdfa01650
Iosif Vieru <iosifvieru440@gmail.com>
2026-01-10 18:05:28 +0200

    add basic register using supabase client

---
+++ frontend/src/auth/auth.js
@@ -0,0 +1,13 @@
+import { supabase } from "./supabaseClient";
+
+export function register(email, password) {
+  return supabase.auth.signUp({ email, password });
+}
+
+export function login(email, password) {
+  return supabase.auth.signInWithPassword({ email, password });
+}
+
+export function logout() {
+  return supabase.auth.signOut();
+}
